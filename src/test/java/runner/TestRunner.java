7d022ee8eb464b6633187a8f2caa6787236de1bc
TRAORE <traoreoumarou97@gmail.com>
2026-04-17 21:25:01 +0200

    added json

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,6 +8,6 @@ import org.junit.platform.suite.api.*;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "steps, hooks")
-@ConfigurationParameter(key = "cucumber.plugin", value = "pretty, html:target/cucumber.html")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty, html:target/cucumber.html,json:target/cucumber.json")
 public class TestRunner {
 }
