e8ebf8ac8925b1dda7617cb9c2c6fcd0765377e0
TRAORE <traoreoumarou97@gmail.com>
2026-04-27 18:29:08 +0200

    added new scenario

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,6 +7,5 @@ import org.junit.platform.suite.api.*;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "steps, hooks")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty, html:target/cucumber.html, json:target/cucumber.json")
-@ConfigurationParameter(key = "cucumber.filter.tags", value = "@checkbox")
 public class TestRunner {
 }
